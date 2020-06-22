3536e0022d5fcda8eddcaa208120e30830ac6022
siddharth <siddharth.shukla@kreativstreet.com>
2020-06-22 20:23:46 +0530

    first commit

---
+++ routes/blog.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+|--------------------------------------------------------------------------
+| Blog Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register blog routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "blog" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    dd('Welcome to blog routes.');
+});
---
+++ routes/manager.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register user routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "user" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    dd('Welcome to manager user routes.');
+});
